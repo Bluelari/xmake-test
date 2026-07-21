ecc78b7fba1d1bb2837b2b3686160b513bcdf433
Bluelari <bluelari@outlook.com>
2026-07-21 09:03:49 +0800

    Add a test library

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,13 @@ set_xmakever("3.0.8")
 
 set_languages("c++20")
 
+target("lib")
+    set_kind("static")
+    add_files("src/lib/**.cpp")
+target_end()
+
 target("main")
     set_kind("binary")
-    add_files("src/**.cpp")
+    add_deps("lib")
+    add_files("src/*.cpp")
 target_end()
